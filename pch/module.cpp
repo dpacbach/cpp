c664caa3acc8b6a47f082ff04ef321de13405927
David P. Sicilia <unspecified>
2017-11-19 17:10:09 -0500

    remove pch folder

--- pch/module.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-#include <algorithm>
-
-#define X 1
-
-void foo(void) {}
--- pch/myheader.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#pragma message("*** myheader.hpp is being compiled")
-
-#define X 1
-
-void foo(void);
