2b79200c9f2c8e4e71fceabb6a766d0382ec9abd
David P. Sicilia <unspecified>
2017-12-08 10:58:23 -0500

    Adding stopwatch functionality

---
+++ util/stopwatch.hpp
@@ -0,0 +1,81 @@
+/****************************************************************
+* StopWatch
+****************************************************************/
+#pragma once
+
+#include <chrono>
+#include <iostream>
+#include <map>
+#include <string>
+#include <string_view>
+#include <vector>
+
+namespace util {
+
+/* This class can be  used  to  mark  start/stop times of various
+ * events and to get the  durations  in  various useful forms. */
+class StopWatch {
+
+public:
+    // For convenience: will start, run, stop.
+    template<typename FuncT>
+    void run( std::string_view name, FuncT func ) {
+        start( name ); func(); stop( name );
+    }
+
+    // Start  the  clock for a given event name. If an event with
+    // this name already exists then  it  will be overwritten and
+    // any end times for it will be deleted.
+    void start( std::string_view name );
+    // Register an end time for an event. Will throw if there was
+    // no start time for the event.
+    void stop( std::string_view name );
+
+    // Get results for an even in the given units.  If  either  a
+    // start or end time for the event has  not  been  registered
+    // then these will throw.
+    int64_t milliseconds( std::string_view name ) const;
+    int64_t seconds     ( std::string_view name ) const;
+    int64_t minutes     ( std::string_view name ) const;
+
+    // Gets the results for an event and then formats them  in  a
+    // way that is most readable given the duration.
+    std::string human( std::string_view name ) const;
+    // Get a list of all results in human  readable  form.  First
+    // element of pair is the event name and the  second  is  the
+    // result of calling human() for that event.
+    using result_pair = std::pair<std::string, std::string>;
+    std::vector<result_pair> results() const;
+
+private:
+    using clock        = std::chrono::system_clock;
+    using time_point   = std::chrono::time_point<clock>;
+    using events_timer = std::map<std::string, time_point>;
+
+    bool event_complete( std::string_view name ) const;
+
+    events_timer start_times;
+    events_timer end_times;
+
+};
+
+/* This is for convenience. Will  start a timer upon construction,
+ * and will stop it upon destruction and print time to stderr. */
+class ScopedWatch {
+
+public:
+    ScopedWatch( std::string_view title )
+        : watch(), name( title ) { watch.start( name ); }
+
+    ~ScopedWatch() {
+        watch.stop( name );
+        std::cerr << name << " time: " << watch.human( name )
+                  << std::endl;
+    }
+
+private:
+    StopWatch   watch;
+    std::string name;
+};
+
+} // namespace util
--- util/util.hpp
+++ util/util.hpp
@@ -10,4 +10,10 @@ namespace util {
 template<typename T>
 std::string to_string( T const& arg );
 
+// Does the set contain the given key.
+template<typename ContainerT, typename KeyT>
+bool has_key( ContainerT const& s, KeyT const& k ) {
+    return s.find( k ) != s.end();
+}
+
 }
