88e1360a8b0985d4bdb4c26fb95fb2ffa687e4eb
David P. Sicilia <unspecified>
2018-01-03 14:41:35 -0500

    Add checkpoint feature to unit tests

--- test/common-test.cpp
+++ test/common-test.cpp
@@ -8,6 +8,13 @@ using namespace std;
 
 namespace testing {
 
+// These are updated on each  macro  call  and are printed out in
+// the  event  that  an exception happens that does not come from
+// one of the unit testing macros  in  order  to help the user to
+// figure out which line threw the exception.
+size_t      checkpoint_line = 0;
+char const* checkpoint_file = "none";
+
 string fail() { return util::c_red    + "Fail" + util::c_norm; }
 string pass() { return util::c_green  + "Pass" + util::c_norm; }
 string skip() { return util::c_yellow + "Skip" + util::c_norm; }
--- test/common-test.hpp
+++ test/common-test.hpp
@@ -14,6 +14,13 @@
 
 namespace testing {
 
+// These are updated on each  macro  call  and are printed out in
+// the  event  that  an exception happens that does not come from
+// one of the unit testing macros  in  order  to help the user to
+// figure out which line threw the exception.
+extern size_t      checkpoint_line;
+extern char const* checkpoint_file;
+
 // This  is the type of a unit test function. All that it does is
 // to  throw an exception if the test fails. If it returns, it is
 // assumed to have passed.
@@ -31,33 +38,50 @@ std::string pass();
 std::string skip();
 std::string bar();
 
+// This  exception is thrown by the unit testing macros to signal
+// a failure so that we  can  distinguish  it from other kinds of
+// generic std::exception's.
+struct failed_exception : public std::exception {
+    failed_exception( std::string const& what )
+        : msg( what ) {}
+    char const* what() const noexcept
+        { return msg.c_str(); }
+    std::string msg;
+};
+
 // This exception is thrown by a  test  to indicate that the test
 // should be skipped.
 struct skipped_exception : public std::exception {};
 
 } // namespace testing
 
-#define TRUE_( a )                                       \
+#define TRUE_( a ) {                                     \
+    testing::checkpoint_line = __LINE__;                 \
+    testing::checkpoint_file = __FILE__;                 \
     if( !(a) ) {                                         \
         using util::operator<<;                          \
         ostringstream ss;                                \
         ss << "On line " << TO_STRING( __LINE__ ) ": ";  \
         ss << "assert " << #a;                           \
-        throw logic_error( ss.str() );                   \
-    }
+        throw failed_exception( ss.str() );              \
+    } }
 
 // a  is an expression that must be true, and b is just something
 // printable that will be displayed  in  the  event  a  is  false.
-#define TRUE( a, b )                                     \
+#define TRUE( a, b ) {                                   \
+    testing::checkpoint_line = __LINE__;                 \
+    testing::checkpoint_file = __FILE__;                 \
     if( !(a) ) {                                         \
         using util::operator<<;                          \
         ostringstream ss;                                \
         ss << "On line " << TO_STRING( __LINE__ ) ": ";  \
         ss << b;                                         \
-        throw logic_error( ss.str() );                   \
-    }
+        throw failed_exception( ss.str() );              \
+    } }
 
-#define EQUALS( a, b )                                        \
+#define EQUALS( a, b ) {                                      \
+    testing::checkpoint_line = __LINE__;                      \
+    testing::checkpoint_file = __FILE__;                      \
     if( !((a) == (b)) ) {                                     \
         using util::operator<<;                               \
         ostringstream ss;                                     \
@@ -65,10 +89,12 @@ struct skipped_exception : public std::exception {};
              << " != " << #b << "\n";                         \
         ss << "instead got: " << (a);                         \
         ss << " ?= " << #b;                                   \
-        throw logic_error( ss.str() );                        \
-    }
+        throw failed_exception( ss.str() );                   \
+    } }
 
-#define THROWS( a )                                  \
+#define THROWS( a ) {                                \
+    testing::checkpoint_line = __LINE__;             \
+    testing::checkpoint_file = __FILE__;             \
     bool STRING_JOIN( __threw_, __LINE__ ) = false;  \
     try {                                            \
         a;                                           \
@@ -76,13 +102,18 @@ struct skipped_exception : public std::exception {};
         STRING_JOIN( __threw_, __LINE__ ) = true;    \
     }                                                \
     TRUE( STRING_JOIN( __threw_, __LINE__ ),         \
-          "expression: " #a " did not throw." );
+          "expression: " #a " did not throw." );     \
+}
 
 // This is used to create a unit test function.
+// TODO: move this code into a cpp function to avoid  duplicating
+// it in test.cpp and to reduce compile times.
 #define TEST( a )                                           \
     void STRING_JOIN( __test_, a )();                       \
     void STRING_JOIN( test_, a )() {                        \
         using util::operator<<;                             \
+        testing::checkpoint_line = __LINE__;                \
+        testing::checkpoint_file = __FILE__;                \
         string test = string( "Test " ) + TO_STRING( a );   \
         cout << left << setw( 40 ) << test;                 \
         enum class Res { PASSED, SKIPPED, FAILED };         \
@@ -93,14 +124,21 @@ struct skipped_exception : public std::exception {};
             result = Res::PASSED;                           \
         } catch( testing::skipped_exception const& ) {      \
             result = Res::SKIPPED;                          \
+        } catch( testing::failed_exception const& e ) {     \
+            err = e.what();                                 \
+            result = Res::FAILED;                           \
         } catch( std::exception const& e ) {                \
             err = e.what();                                 \
+            err += "\nLast checkpoint: ";                   \
+            err += std::string( checkpoint_file );          \
+            err += ", line ";                               \
+            err += util::to_string( checkpoint_line );      \
             result = Res::FAILED;                           \
         } catch( ... ) {                                    \
             err = "unknown exception";                      \
             result = Res::FAILED;                           \
         }                                                   \
-        cerr << "   | ";                                    \
+        cerr << "     | ";                                  \
         bool failed = false;                                \
         switch( result ) {                                  \
         case Res::FAILED:                                   \
