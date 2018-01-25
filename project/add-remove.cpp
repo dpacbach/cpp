a6d3b8d912f10def98d667120f598b4332d7719b
David P. Sicilia <unspecified>
2018-01-25 13:14:32 -0500

    Finish off add_2_vcxproj and rm_from

--- project/add-remove.cpp
+++ project/add-remove.cpp
@@ -10,61 +10,73 @@
 
 using namespace std;
 
+using pugi::xml_document;
+using pugi::xml_node;
+
 namespace project {
 
 namespace {
 
-string to_project( fs::path const& proj_file,
-                   fs::path const& src_file ) {
+// Takes a project file which must exist and, if relative, is  as-
+// sumed relative to CWD. Then takes a source file which may  not
+// exist  and,  if  relative, is assumed relative to CWD. It will
+// make the source file relative to  the  folder  containing  the
+// project file and convert slashes to backslashes and return the
+// result as a string. The reason for the string result  is  that,
+// on some platforms, it would not work to convert  a  path  with
+// backslashes to an fs::path.
+string to_prj( fs::path const& prj, fs::path const& src ) {
 
-    auto proj_folder =
-        util::absnormpath( proj_file ).parent_path();
-
-    auto src = util::back_slashes(
+    return util::back_slashes(
         util::lexically_relative(
-            util::absnormpath( src_file ),
-            proj_folder
+            util::lexically_absolute( src ),
+            util::absnormpath( prj ).parent_path()
         ).string()
     );
-    return src;
 }
 
-fs::path from_project( fs::path const& proj_file,
-                       string   const& src_win ) {
+// Takes a project file which must exist and, if relative, is  as-
+// sumed relative to CWD. Then takes a string whose contents  rep-
+// resent a source file path as  it would appear inside a project
+// file (i.e., possibly absolute, or possibly relative to project
+// file location, and with backslashes). The  return  is  the  ab-
+// solute  (native)  path to the source file, which may not exist,
+// on the filesystem.
+fs::path from_prj( fs::path const& prj, string const& src_win ) {
 
     auto src = fs::path( util::fwd_slashes( src_win ) );
-
-    auto proj_folder =
-        util::absnormpath( proj_file ).parent_path();
-
-    return util::lexically_normal( proj_folder/src);
+    auto prj_dir = util::absnormpath( prj ).parent_path();
+    return util::lexically_normal( util::slash( prj_dir, src ) );
 }
 
-bool match_src( fs::path const& proj_file,
+// Given a project (represented by the given project file), would
+// the  string  p2, if it were inside the project file, reference
+// the file p1? p1 may not exist and, if it is  relative,  is  as-
+// sumed  relative  to CWD. p2 can be a literal string taken from
+// within  a  project file, i.e., may contain backslashes, may be
+// absolute  or  relative, and if relative is assumed relative to
+// location of project folder.
+bool match_src( fs::path const& prj,
                 fs::path const& p1,
-                string   const& p2_win ) {
+                string   const& p2 ) {
 
-    auto p1_abs = p1;
-
-    if( !p1_abs.is_absolute() )
-        p1_abs = util::lexically_normal( fs::current_path()/p1 );
-
-    auto p2 = from_project( proj_file, p2_win );
-
-    return util::iequals( p1_abs.string(), p2.string() );
+    return util::path_equals( util::lexically_absolute( p1 ),
+                              from_prj( prj, p2 ),
+                              util::CaseSensitive::NO );
 }
 
-vector<pugi::xml_node> find_src( pugi::xml_node const& node,
-                                 fs::path       const& proj_file,
-                                 fs::path       const& src ) {
+// Starting from the given node, traverse the tree and look for a
+// reference to a  source  file  that  corresponds  to  the given
+// source  file  in the sense that the normalized, absolute paths
+// should match in a case-insensitive way.
+vector<xml_node> find_src( xml_node const& node,
+                           fs::path const& prj,
+                           fs::path const& src ) {
 
-    return xml::filter( node, [&]( pugi::xml_node n ) {
-
-        auto Include = xml::attr( n, "@Include" );
-        return Include &&
-               string( n.name() ) == "ClCompile" &&
-               match_src( proj_file, src, *Include );
-
+    return xml::filter( node, [&]( xml_node n ) {
+        auto Inc = xml::attr( n, "@Include" );
+        return Inc && string( n.name() ) == "ClCompile" &&
+                      match_src( prj, src, *Inc );
     } );
 }
 
@@ -76,43 +88,41 @@ vector<pugi::xml_node> find_src( pugi::xml_node const& node,
 // project file, regardless of platform.  The  source file, if it
 // is  a  relative  path, is interpreted as being relative to CwD
 // (not relative to the  project  file  location).  If the source
-// file already exists in the project file then an exception will
-// be thrown.
-void add_2_vcxproj( fs::path const& proj_file,
+// file  already  exists in the project file (by case-insensitive
+// comparison) then an exception will be thrown. The source  file
+// does not need to exist.
+void add_2_vcxproj( fs::path const& prj,
                     fs::path const& src_file ) {
 
-    pugi::xml_document doc; xml::parse( doc, proj_file );
+    xml_document doc; xml::parse( doc, prj );
 
-    ASSERT( find_src( doc, proj_file, src_file ).empty(),
-           "Project file " << proj_file << " already contains "
-           "source file "  << src_file );
+    ASSERT( find_src( doc, prj, src_file ).empty(),
+            "Project file " << prj << " already contains "
+            "source file "  << src_file );
 
+    // See if  there  is  already  an  ItemGroup  containing some
+    // ClCompiles. If so then use it,  otherwise we will create a
+    // new ItemGroup.
     auto ItemGroup = xml::xpath( "//ItemGroup[ClCompile]", doc );
 
-    pugi::xml_node parent = ItemGroup.empty()
-                          ? xml::node( doc, "/Project" )
-                            .append_child( "ItemGroup" )
-                          : ItemGroup[0].node();
+    // Whether we use an existing one or create a new one, get  a
+    // reference to the ItemGroup under which we will add the new
+    // source file.
+    xml_node parent = ItemGroup.empty()
+                    ? xml::node( doc, "/Project" )
+                      .append_child( "ItemGroup" )
+                    : ItemGroup[0].node();
 
+    // E.g., produces: <ClCompile Include="../../A.cpp" />
     parent.append_child( "ClCompile" )
           .append_attribute( "Include" ) =
-           to_project( proj_file, src_file ).c_str();
+           to_prj( prj, src_file ).c_str();
 
-    auto name = proj_file.string();
-    ASSERT( doc.save_file( name.c_str(), "  " ), "failed to "
-            "save document to file " << name );
+    // Rewrite the entire project file, with  the  new  ClCompile.
+    ASSERT( doc.save_file( prj.string().c_str(), "  " ),
+            "failed to save document to file " << prj );
 }
 
-// Open  the  filters  file and insert the given source file into
-// it. First we search the filters  file for any other files that
-// are  in the same folder as the given one and, if there are, we
-// will obtain the name of the filter in that way by choosing the
-// first filter that we encounter that contains at least one file
-// in the same folder  as  src_file.  Otherwise,  we will use the
-// first filter that we find. The source file, if it  is  a  rela-
-// tive path, is interpreted as being relative to CWD  (not  rela-
-// tive to the project file location). If the source file already
-// exists in the filters file  then  an  exception will be thrown.
 void add_2_filters( fs::path const& filters_file,
                     fs::path const& src_file ) {
     (void)filters_file;
@@ -122,11 +132,13 @@ void add_2_filters( fs::path const& filters_file,
 // This  is simply for convenience; it will call add_2_project on
 // the given project file,  then  will  construct  a filters file
 // path by appending ".filters" to the  end  of the name and will
-// then call add_2_filters on the filters file.
-void add_2_project( fs::path const& proj_file,
+// then  call  add_2_filters  on  the filters file. If either the
+// project file or filters file does  not  exist,  and  exception
+// will be thrown. The source file does not need to exist.
+void add_2_project( fs::path const& prj,
                     fs::path const& src_file ) {
-    add_2_vcxproj( proj_file, src_file );
-    auto filters = proj_file;
+    add_2_vcxproj( prj, src_file );
+    auto filters = prj;
     filters += ".filters";
     add_2_filters( filters, src_file );
 }
@@ -139,12 +151,29 @@ void add_2_project( fs::path const& proj_file,
 // paths  encountered  in  the file will be normalized before per-
 // forming  a case-insensitive comparison to find a match. If the
 // source file is not found in the file then an exception will be
-// thrown.
+// thrown.  Also,  the source file need not exist in the file sys-
+// tem; this is to accomodate  cases  where  we  want to remove a
+// file from a project file  because  the  user  has  it  locally
+// deleted in their working copy.
 void rm_from( fs::path const& file,
-              fs::path const& src_file ) {
-    (void)file;
-    (void)src_file;
-}
+              fs::path const& src ) {
 
+    xml_document doc; xml::parse( doc, file );
+
+    auto where = find_src( doc, file, src );
+
+    ASSERT( !where.empty(), "Project file " << file << " does "
+            "not contain source file " << src );
+
+    // In theory, there could be multiple references to the  same
+    // source file, so we just remove  all of them (although this
+    // should not happen in practice).
+    for( auto n : where )
+        n.parent().remove_child( n );
+
+    // Rewrite the entire project file, with  the  new  ClCompile.
+    ASSERT( doc.save_file( file.string().c_str(), "  " ),
+            "failed to save document to file " << file );
+}
 
 } // namespace project
--- project/add-remove.hpp
+++ project/add-remove.hpp
@@ -13,8 +13,9 @@ namespace project {
 // project file, regardless of platform.  The  source file, if it
 // is  a  relative  path, is interpreted as being relative to CwD
 // (not relative to the  project  file  location).  If the source
-// file already exists in the project file then an exception will
-// be thrown.
+// file  already  exists in the project file (by case-insensitive
+// comparison) then an exception will be thrown. The source  file
+// does not need to exist.
 void add_2_vcxproj( fs::path const& project_file,
                     fs::path const& src_file );
 
@@ -27,14 +28,18 @@ void add_2_vcxproj( fs::path const& project_file,
 // first filter that we find. The source file, if it  is  a  rela-
 // tive path, is interpreted as being relative to CWD  (not  rela-
 // tive to the project file location). If the source file already
-// exists in the filters file  then  an  exception will be thrown.
+// exists in the  filters  file  (by case-insensitive comparison)
+// then an exception will be thrown. The  source  file  does  not
+// need to exist.
 void add_2_filters( fs::path const& filters_file,
                     fs::path const& src_file );
 
 // This  is simply for convenience; it will call add_2_project on
 // the given project file,  then  will  construct  a filters file
 // path by appending ".filters" to the  end  of the name and will
-// then call add_2_filters on the filters file.
+// then  call  add_2_filters  on  the filters file. If either the
+// project file or filters file does  not  exist,  and  exception
+// will be thrown. The source file does not need to exist.
 void add_2_project( fs::path const& project_file,
                     fs::path const& src_file );
 
@@ -46,7 +51,10 @@ void add_2_project( fs::path const& project_file,
 // paths  encountered  in  the file will be normalized before per-
 // forming  a case-insensitive comparison to find a match. If the
 // source file is not found in the file then an exception will be
-// thrown.
+// thrown.  Also,  the source file need not exist in the file sys-
+// tem; this is to accomodate  cases  where  we  want to remove a
+// file from a project file  because  the  user  has  it  locally
+// deleted in their working copy.
 void rm_from( fs::path const& file,
               fs::path const& src_file );
 
