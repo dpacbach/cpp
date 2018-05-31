3068998a003d8bbfddb4f9dfd7f9612a03445eaa
David P. Sicilia <unspecified>
2018-05-31 17:18:07 -0400

    Fixing bugs in YCM py file

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -1,16 +1,20 @@
-from os.path import basename, splitext
+from os import environ
+from os.path import basename, splitext, relpath, dirname
 from subprocess import Popen, PIPE
 
-LOG_FILE = '/home/dsicilia/ycm_log.txt'
+LOG_FILE = "%s/ycm_log.txt" % environ['HOME']
 
 def make( filename ):
-    stem = basename( splitext( filename )[0] )
-    target = '.lib-linux64/%s.o' % stem
+    rel_file = relpath( filename )
+    rel_dir  = dirname( relpath( filename ) )
+    rel_dir = (rel_dir+'/') if rel_dir else ''
+    stem = basename( splitext( rel_file )[0] )
+    target = '%s.lib-linux64/%s.o' % (rel_dir, stem)
     # -n means don't actually run the target, just print the commands,
     # -B means act as if the target were out of date even if it is not
-    p = Popen( ['/usr/bin/make', 'CLANG=', '-nB', 'V=', target], stdout=PIPE )
+    p = Popen( ['/usr/bin/make', 'CLANG=', '-nB', 'V=', target], stdout=PIPE, stderr=PIPE )
     (stdout, stderr) = p.communicate()
-    assert p.returncode == 0, 'return code non-zero'
+    assert p.returncode == 0, 'return code non-zero:\n%s' % stderr
     lines = [l for l in stdout.split( '\n' ) if target in l]
     assert len( lines ) == 1, 'length == %d' % len( lines )
     return lines[0]
