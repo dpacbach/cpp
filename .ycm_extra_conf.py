233393146b34cd59f61aeef4dd82f7bb86c33a17
David P. Sicilia <unspecified>
2018-06-09 17:29:33 -0400

    Improving .ycm_extra_conf.py

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -1,27 +1,46 @@
-from os import environ
-from os.path import basename, splitext, relpath, dirname
-from subprocess import Popen, PIPE
+from os.path import relpath, dirname, splitext, basename, join
+import subprocess as sp
 
-LOG_FILE = "%s/ycm_log.txt" % environ['HOME']
+DEBUG    = False
+LOG_FILE = "/tmp/ycm-extra-conf-log.txt"
+LIB_DIR  = '.lib-linux64'
 
-def make( filename ):
-    rel_file = relpath( filename )
-    rel_dir  = dirname( relpath( filename ) )
-    rel_dir = (rel_dir+'/') if rel_dir else ''
-    stem = basename( splitext( rel_file )[0] )
-    target = '%s.lib-linux64/%s.o' % (rel_dir, stem)
-    # -n means don't actually run the target, just print the commands,
-    # -B means act as if the target were out of date even if it is not
-    p = Popen( ['/usr/bin/make', 'CLANG=', '-nB', 'V=', target], stdout=PIPE, stderr=PIPE )
+def _run_cmd( cmd ):
+    p = sp.Popen( cmd, stdout=sp.PIPE, stderr=sp.PIPE )
     (stdout, stderr) = p.communicate()
-    assert p.returncode == 0, 'return code non-zero:\n%s' % stderr
-    lines = [l for l in stdout.split( '\n' ) if target in l]
-    assert len( lines ) == 1, 'length == %d' % len( lines )
-    return lines[0]
+    assert p.returncode == 0, 'error:\n%s' % stderr
+    return stdout
+
+def _make( path ):
+
+    # Convert file to object file, put it inside the lib folder,
+    # and then make it relative to current directory, e.g.:
+    #
+    #    /A/B/C/file.cpp ==> ../../C/.lib-linux64/file.o
+
+    rel_file = relpath( path )
+    rel_dir  = dirname( rel_file )
+    stem     = basename( splitext( rel_file )[0] )
+    target   = join( rel_dir, LIB_DIR, stem) + '.o'
+
+    # -n : don't run the target, just print commands,
+    # -B : act as if the target were out of date
+    # V= : tells nr-make to not suppress command echoing
+    cmd = ['/usr/bin/make', 'CLANG=', '-nB', 'V=', target]
+
+    stdout = _run_cmd( cmd )
+
+    (line,) = [l for l in stdout.split( '\n' ) if target in l]
+
+    return line
 
 def FlagsForFile( filename, **kwargs ):
-    with file( LOG_FILE, 'a' ) as f:
-        f.write( "-\nfilename: %s\n" % filename )
-        flags = make( filename )
-        f.write( "cmd:      %s\n" % flags )
+
+    flags = _make( filename )
+
+    if DEBUG:
+        with file( LOG_FILE, 'a' ) as f:
+            f.write( "-\nfilename: %s\n" % filename )
+            f.write( "cmd:      %s\n" % flags )
+
     return { 'flags': flags.split() }
